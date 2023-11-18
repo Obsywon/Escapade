749a1dd242c216a3f4bb60e7225636398abcbf5c
Guillaume V <obsywon@gmail.com>
2023-11-18 10:47:13 +0100

    feat(ui): ajout navigation / layouts (invité & connecté) + layout pour formulaire

---
+++ frontend/layouts/FormLayout.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Keyboard,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+interface FormLayoutProps {
+  children: React.ReactElement;
+}
+
+/**
+ * Permet à l'affichage de s'adapter lorsqu'un formulaire nécessite l'apparition du clavier
+ * @param FormLayoutProps enfants react native à afficher dans le formulaire
+ * @returns Un formulaire qui s'adapte à l'apparition du clavier
+ */
+function FormLayout({children}: FormLayoutProps): JSX.Element {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        {children}
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default FormLayout;
--- frontend/pages/InscriptionScreen.tsx
+++ frontend/pages/InscriptionScreen.tsx
@@ -1,44 +1,39 @@
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {Surface} from 'react-native-paper';
 import MainTitle from '../components/MainTitle';
 import PasswordInput from '../components/PasswordInput';
 import BasicTextInput from '../components/BasicTextInput';
 import BasicButton from '../components/BasicButton';
 import AppTitle from '../components/AppTitle';
+import FormLayout from '../layouts/FormLayout';
 
-function InscriptionForm(): JSX.Element {
+function InscriptionScreen(): JSX.Element {
   const [password, setPassword] = useState<string | undefined>();
   const [email, setEmail] = useState<string | undefined>();
 
   return (
-    <Surface style={styles.container} elevation={2}>
-      <View style={styles.formContainer}>
-      <AppTitle title="Escapade" />
+    <FormLayout>
+      <Surface style={styles.formContainer} elevation={2}>
+        <AppTitle title="Escapade" />
         <MainTitle title="Inscription" />
         <BasicTextInput value={email} setValue={setEmail} label="E-mail" />
         <PasswordInput value={password} setPassword={setPassword} />
         <BasicButton label="Inscription" />
-      </View>
-    </Surface>
+      </Surface>
+    </FormLayout>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    borderRadius: 16,
-    padding: 16,
-  },
   formContainer: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
+    padding: 24,
     paddingTop: 72,
     paddingBottom: 72,
   },
 });
 
-export default InscriptionForm;
+export default InscriptionScreen;
