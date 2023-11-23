251a214eff065f32bdfb923c2d6505244b10c2e6
Guillaume V <obsywon@gmail.com>
2023-11-23 21:35:23 +0100

    feat(ui): prépare validation formulaire

--- frontend/pages/ConnexionScreen.tsx
+++ frontend/pages/ConnexionScreen.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, ScrollView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {Surface} from 'react-native-paper';
 import AppTitle from '../components/AppTitle';
 import MainTitle from '../components/MainTitle';
@@ -11,29 +11,32 @@ import BasicButton from '../components/forms/BasicButton';
 export default function ConnexionScreen(): JSX.Element {
   const [email, setEmail] = useState<string | undefined>();
   const [password, setPassword] = useState<string | undefined>();
+  const [sendable, setSendable] = useState<boolean>(false);
+
+  useEffect(() => {
+    let canSend: boolean = false;
+    if (password != null && password.length >= 8) {
+      canSend = true;
+    }
+
+    setSendable(canSend);
+  }, [email, password]);
 
   return (
     <FormLayout>
       <Surface style={styles.formContainer} elevation={2}>
-        <ScrollView
-          maximumZoomScale={1}
-          minimumZoomScale={1}
-          automaticallyAdjustContentInsets={true}
-          automaticallyAdjustsScrollIndicatorInsets={true}
-          pagingEnabled={true}
-          contentContainerStyle={styles.scrollViewContent}>
-          <View style={styles.titles}>
-            <AppTitle title="Escapade" />
-            <MainTitle title="Inscription" />
-            <BasicTextInput value={email} setValue={setEmail} label="E-mail" />
-            <PasswordInput
-              value={password}
-              setPassword={setPassword}
-              label="Mot de passe"
-            />
-          </View>
-          <BasicButton label="Connexion" disabled={true} />
-        </ScrollView>
+        <View style={styles.form}>
+          <AppTitle title="Escapade" />
+          <MainTitle title="Inscription" />
+
+          <BasicTextInput value={email} setValue={setEmail} label="E-mail" />
+          <PasswordInput
+            value={password}
+            setPassword={setPassword}
+            label="Mot de passe"
+          />
+          <BasicButton label="Connexion" disabled={!sendable} />
+        </View>
       </Surface>
     </FormLayout>
   );
@@ -46,19 +49,9 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     padding: 0,
   },
-  linedInputs: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-  scrollView: {
-    flex: 1,
-  },
-  scrollViewContent: {
-    padding: 8,
-    paddingRight: 24,
-    paddingLeft: 24,
-  },
-  titles: {
-    flex: 1,
+  form: {
+    height: '70%',
+    minHeight: '60%',
+    paddingHorizontal: 24,
   },
 });
