f7c456b2abbcc5e4ebef8c944dbd3bfc80093f61
CathDen <75126396+CathDen@users.noreply.github.com>
2023-12-10 09:45:09 +0100

    Mise en place Navigation

---
+++ frontend/navigation/AppNavigator.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import pageBienvenue from '../pages/pageBienvenue';
+import InscriptionScreen from '../pages/InscriptionScreen';
+// import pageConnexion from '../pages/pageConnexion';
+
+
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="pageBienvenue">
+        <Stack.Screen name="pageBienvenue" component={pageBienvenue} />
+        <Stack.Screen name="InscriptionScreen" component={InscriptionScreen} />
+        {/* <Stack.Screen name="pageConnexion" component={pageConnexion} /> */}
+        {/* Ajoutez d'autres écrans ici si nécessaire */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
