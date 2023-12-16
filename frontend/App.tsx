c96bc48673f2bb3e7d26622c7a968805cf86f4b2
TheoRousseau25 <theo.rousseau@gmx.fr>
2023-12-16 20:29:33 +0100

    Lien entre front et back lors de l'inscription

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,16 +5,24 @@ import GuestLayout from './layouts/GuestLayout';
 import ConnectedLayout from './layouts/ConnectedLayout';
 import { NavigationContainer } from '@react-navigation/native';
 import { PaperProvider } from 'react-native-paper';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://func-escapade-dev-fc.azurewebsites.net/api/graphql/',
+  cache: new InMemoryCache(),
+});
 
 function App(): JSX.Element {
   const [connected, setConnected] = useState<boolean>(false);
 
   return (
-    <PaperProvider>
-        <NavigationContainer>
-          { connected ? <ConnectedLayout /> : <GuestLayout />}
-        </NavigationContainer>
-      </PaperProvider>
+      <ApolloProvider client={client}>
+        <PaperProvider>
+          <NavigationContainer>
+            { connected ? <ConnectedLayout /> : <GuestLayout />}
+          </NavigationContainer>
+        </PaperProvider>
+      </ApolloProvider>
   )
 }
 
