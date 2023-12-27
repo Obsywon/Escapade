81c5fe661815855fd59cb9ee119cba1d9e2b260e
CathDen <75126396+CathDen@users.noreply.github.com>
2023-12-27 16:23:17 +0100

    avant modification Guillaume

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,47 @@
-
-import 'react-native-gesture-handler';
-import React, {useState} from 'react';
-import GuestLayout from './layouts/GuestLayout';
-import ConnectedLayout from './layouts/ConnectedLayout';
+import React, { useState, useEffect } from 'react';
+import { Text } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PaperProvider } from 'react-native-paper';
+import GuestLayout from './layouts/GuestLayout';
+import ConnectedLayout from './layouts/ConnectedLayout';
 import pageBienvenue from './pages/pageBienvenue';
 import InscriptionScreen from './pages/InscriptionScreen';
 
+
 const Stack = createNativeStackNavigator();
 
-function App(): JSX.Element {
+const fetchFonts = () => {
+  return Font.loadAsync({
+    // Définissez ici vos différentes polices
+    'Fontastique': require('./assets/fonts/Fontastique.ttf'),
+  });
+};
+
+const App: React.FC = () => {
+  const [dataLoaded, setDataLoaded] = useState(false);
   const [connected, setConnected] = useState<boolean>(false);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchFonts();
+      setDataLoaded(true);
+    };
 
+    fetchData();
+  }, []);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.error(err)}
+      />
+    );
+  }
 
   return (
     <PaperProvider>
@@ -31,8 +58,6 @@ function App(): JSX.Element {
       </NavigationContainer>
     </PaperProvider>
   );
-}
+};
 
 export default App;
-
-
