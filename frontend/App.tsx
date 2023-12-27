4b7316e7fe61252dd8e2fe7c974eeeabb9748897
Guillaume V <obsywon@gmail.com>
2023-12-27 20:27:38 +0100

    correction bugs connexionscreen / app (affichage écrasée)

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,28 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
-import * as Font from 'expo-font';
+import React, { useState, useEffect } from "react";
+import { Text } from "react-native";
+import * as Font from "expo-font";
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 
-import { PaperProvider } from 'react-native-paper';
-import GuestLayout from './layouts/GuestLayout';
-import ConnectedLayout from './layouts/ConnectedLayout';
-
+import { PaperProvider } from "react-native-paper";
+import GuestLayout from "./layouts/GuestLayout";
+import ConnectedLayout from "./layouts/ConnectedLayout";
 
 import { CustomTheme } from "./themes/CustomTheme";
 import useCustomFonts from "./hooks/useCustomFonts";
 import LoadingSurface from "./components/LoadingSurface";
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import pageBienvenue from './pages/pageBienvenue';
-import InscriptionScreen from './pages/InscriptionScreen';
-import ConnexionScreen from './pages/ConnexionScreen';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import pageBienvenue from "./pages/pageBienvenue";
+import InscriptionScreen from "./pages/InscriptionScreen";
+import ConnexionScreen from "./pages/ConnexionScreen";
 
 const Stack = createNativeStackNavigator();
 const client = new ApolloClient({
-  uri: 'https://func-escapade-dev-fc.azurewebsites.net/api/graphql/',
+  uri: "https://func-escapade-dev-fc.azurewebsites.net/api/graphql/",
   cache: new InMemoryCache(),
 });
 
@@ -33,7 +32,7 @@ function App(): JSX.Element {
   if (!fontLoaded) {
     return (
       <ApolloProvider client={client}>
-        <PaperProvider theme={{ ...CustomTheme, fonts }}>
+        <PaperProvider theme={CustomTheme}>
           <LoadingSurface text="Chargement en cours..." />
         </PaperProvider>
       </ApolloProvider>
@@ -47,16 +46,24 @@ function App(): JSX.Element {
           {connected ? (
             <ConnectedLayout />
           ) : (
-            <Stack.Navigator initialRouteName="pageBienvenue">
-              <Stack.Screen name="pageBienvenue" component={pageBienvenue} />
-              <Stack.Screen name="InscriptionScreen" component={InscriptionScreen} />
-              <Stack.Screen name="ConnexionScreen" component={ConnexionScreen} />
-            </Stack.Navigator>
+            <GuestLayout>
+              <Stack.Navigator initialRouteName="pageBienvenue">
+                <Stack.Screen name="pageBienvenue" component={pageBienvenue} />
+                <Stack.Screen
+                  name="InscriptionScreen"
+                  component={InscriptionScreen}
+                />
+                <Stack.Screen
+                  name="ConnexionScreen"
+                  component={ConnexionScreen}
+                />
+              </Stack.Navigator>
+            </GuestLayout>
           )}
         </NavigationContainer>
       </PaperProvider>
     </ApolloProvider>
-  )
+  );
 }
 
 export default App;
