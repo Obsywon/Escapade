b5fcefd9366328fedea437b0c9f6b86dc392023f
CathDen <75126396+CathDen@users.noreply.github.com>
2024-01-03 04:52:46 +0100

    mise en place navigation menu bas 	modified:   frontend/App.tsx 	modified:   frontend/navigation/AppNavigator.tsx 	new file:   frontend/navigation/TabNavigator.tsx 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	new file:   frontend/pages/Accueil.tsx 	renamed:    frontend/pages/pageBienvenue.tsx -> frontend/pages/Bienvenue.tsx 	new file:   frontend/pages/Favori.tsx 	new file:   frontend/pages/Photos.tsx 	new file:   frontend/pages/Profile.tsx 	new file:   frontend/pages/Recherche.tsx 	deleted:    frontend/pages/pageConnexion.tsx 	deleted:    frontend/pages/pageProfile.tsx 	modified:   frontend/yarn.lock

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -37,9 +37,11 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 >>>>>>> 4b7316e (correction bugs connexionscreen / app (affichage écrasée))
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import pageBienvenue from "./pages/pageBienvenue";
+
 import InscriptionScreen from "./pages/InscriptionScreen";
 import ConnexionScreen from "./pages/ConnexionScreen";
+import pageBienvenue from "./pages/Bienvenue";
+
 
 const Stack = createNativeStackNavigator();
 const client = new ApolloClient({
