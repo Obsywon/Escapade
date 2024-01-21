a91beee1f0112285eb8ecccebd065263b4a913e5
TheoRousseau25 <theo.rousseau@gmx.fr>
2024-01-21 16:56:26 +0100

    Maj d'accueil

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,27 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Text } from "react-native";
 import * as Font from "expo-font";
 
-<<<<<<< HEAD
-import { NavigationContainer } from '@react-navigation/native';
-
-import { PaperProvider } from 'react-native-paper';
-import GuestLayout from './layouts/GuestLayout';
-import ConnectedLayout from './layouts/ConnectedLayout';
-import pageBienvenue from './pages/pageBienvenue';
-import InscriptionScreen from './pages/InscriptionScreen';
-import {CustomTheme} from "./themes/CustomTheme";
-import useCustomFonts from "./hooks/useCustomFonts";
-import LoadingSurface from "./components/LoadingSurface";
-
-const Stack = createNativeStackNavigator();
-
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://func-escapade-dev-fc.azurewebsites.net/api/graphql/',
-  cache: new InMemoryCache(),
-});
-=======
 import { NavigationContainer } from "@react-navigation/native";
 
 import { PaperProvider } from "react-native-paper";
@@ -34,7 +13,6 @@ import useCustomFonts from "./hooks/useCustomFonts";
 import LoadingSurface from "./components/LoadingSurface";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
->>>>>>> 4b7316e (correction bugs connexionscreen / app (affichage écrasée))
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
@@ -49,24 +27,8 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    // Définissez ici vos différentes polices
-    'Fontastique': require('./assets/fonts/Fontastique.ttf'),
-  });
-};
-
-const App: React.FC = () => {
-  const [dataLoaded, setDataLoaded] = useState(false);
+function App(): JSX.Element {
   const [connected, setConnected] = useState<boolean>(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetchFonts();
-      setDataLoaded(true);
-    };
-
-
   const [fonts, fontLoaded] = useCustomFonts();
 
   if (!fontLoaded) {
@@ -81,14 +43,6 @@ const App: React.FC = () => {
 
   return (
     <ApolloProvider client={client}>
-<<<<<<< HEAD
-      <PaperProvider theme={{...CustomTheme, fonts}}>
-          <NavigationContainer>
-            { connected ? <ConnectedLayout /> : <GuestLayout />}
-          </NavigationContainer>
-        </PaperProvider>
-      </ApolloProvider>
-=======
       <PaperProvider theme={{ ...CustomTheme, fonts }}>
         <NavigationContainer>
           {connected ? (
@@ -111,12 +65,7 @@ const App: React.FC = () => {
         </NavigationContainer>
       </PaperProvider>
     </ApolloProvider>
-<<<<<<< HEAD
->>>>>>> b01440e (page connexion à résoudre)
-  )
-=======
   );
->>>>>>> 4b7316e (correction bugs connexionscreen / app (affichage écrasée))
 }
 
 export default App;
