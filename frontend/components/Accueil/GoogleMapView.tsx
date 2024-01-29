c77547144ce770921fa03b395c779f8b4e8826fa
TheoRousseau25 <theo.rousseau@gmx.fr>
2024-01-29 08:47:53 +0100

    Ajout navigation

--- frontend/components/Accueil/GoogleMapView.tsx
+++ frontend/components/Accueil/GoogleMapView.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, Linking, TouchableOpacity } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE, Region } from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import PlaceMarker from './PlaceMarker';
 import { API_KEY } from '../../services/GlobaleApi';
 import { UserLocationContextType, UserLocationContext } from '../../contexts/UserLocationContext';
+import CustomMarkerImage from '../../assets/logo.png';
 
 interface GoogleMapViewProps {
   placeList: any[];
@@ -47,12 +48,19 @@ export default function GoogleMapView({ placeList, transportMode }: GoogleMapVie
 
   const waypoints = placeList.slice(0, 4).map(extractCoordinates);
 
+  const handleNavigatePress = () => {
+    const waypointCoords = waypoints.map((waypoint) => `${waypoint.latitude},${waypoint.longitude}`).join('|');
+    const navigationURL = `https://www.google.com/maps/dir/?api=1&origin=${startPoint?.latitude},${startPoint?.longitude}&destination=${startPoint?.latitude},${startPoint?.longitude}&waypoints=${waypointCoords}`;
+  
+    Linking.openURL(navigationURL);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.texteTitre}>Meilleurs endroits à proximité</Text>
       <View style={styles.mapContainer}>
         <MapView style={styles.map} provider={PROVIDER_GOOGLE} showsUserLocation={true} region={mapRegion}>
-          {startPoint && <Marker coordinate={startPoint} />}
+          {startPoint && <Marker coordinate={startPoint} image={CustomMarkerImage} />}
           {placeList.map((item, index) => index < 4 && <PlaceMarker key={index} item={item} />)}
           <MapViewDirections
             origin={startPoint}
@@ -65,6 +73,9 @@ export default function GoogleMapView({ placeList, transportMode }: GoogleMapVie
           />
         </MapView>
       </View>
+      <TouchableOpacity style={styles.navigateButton} onPress={handleNavigatePress}>
+        <Text style={styles.navigateButtonText}>Naviguer</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -86,5 +97,16 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('screen').width * 0.89,
     height: Dimensions.get('screen').height * 0.23,
+  },
+  navigateButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  navigateButtonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
